6d4e8580204a71f84c43c229704552d678e2e02e
Pranav-8bit <tpranav15@gmail.com>
2022-12-22 18:58:38 +0530

    component setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//components
+import Sidebar from "./components/sidebar/Sidebar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Blog from "./components/blog/Blog";
+import Home from "./components/home/Home";
+import Contact from "./components/contact/Contact";
+import Resume from "./components/resume/Resume";
+import About from "./components/about/About";
+import Testimonial from "./components/testimonials/Testimonial";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonial />
+        <Blog />
+        <Contact />
+      </main>
+    </>
   );
 }
 
