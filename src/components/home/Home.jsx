99b00c27fb1308edcc1159fde50897be0ca480eb
Pranav tiwari <67794060+Pranav-8bit@users.noreply.github.com>
2024-03-17 11:53:08 +0530

    profile photo update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
-        <img src="https://cdn.discordapp.com/attachments/736227949019398235/1059156928309252106/myimage.png" alt="Me" className="home__img" />
+        <img src="https://cdn.discordapp.com/attachments/736227949019398235/1218806574521385020/1701497958059.jpeg?ex=66090113&is=65f68c13&hm=51c9989b16288736aa7b8ba5db404525044e3040ecc0c0f7ebddbee929db259d&" alt="Me" className="home__img" />
         <h1 className="home__name">Pranav Tiwari</h1>
         <span className="home__education">
         I'm a Frontend Enthusiast and a learner of web development.
